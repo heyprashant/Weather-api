b3d73b92a664acdca96d07c3d03754163ecf7703
heyprashant <prashant.p7804@gmail.com>
2020-12-18 20:36:17 +0530

    intigrate Weather and geocode APIS

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Scranton', (error, data)=>{
-    console.log('Error', error);
-    console.log('Data', data);
+const location = process.argv[2];
+
+if(!location)
+    return console.log('Please provide a location.');
+
+geocode(location, (error, data)=>{
+    if (error) {
+       return console.log(error);
+    }
+    //callback chaining
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log(data.location);
+        console.log(forecastData);
+      });
 });
 
-forecast(41.4087, -75.6621, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
