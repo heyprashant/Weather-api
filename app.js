b8c0721b0a9d1e49e6e6aadb0c8ea54b5b6dbb2f
heyprashant <prashant.p7804@gmail.com>
2020-12-18 13:17:13 +0530

    Add geoCode API -> WeatherStack API

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ const url = 'http://api.weatherstack.com/current?access_key=b5b9c3b9e7791900c6f3
 request( { url : url, json: true }, ( error, response )=> {
     console.log( response.body.current.weather_descriptions[0]+'. It is currently',response.body.current.temperature,'degrees out. It feels like',response.body.current.feelslike,'degrees out');
 });
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/bhiwani.json?access_token=pk.eyJ1IjoiaGV5cHJhc2hhbnQiLCJhIjoiY2tpdHd1bjg5MDU2OTJxbXd4c2pxYWF3NSJ9.6j0sGJDAiPl51skW7CEarQ&limit=1';
+
+request ( { url: geocodeURL, json: true }, ( error, response )=>{
+    console.log('Lattitude:', response.body.features[0].center[1]);
+    console.log('Longitude:',response.body.features[0].center[0]);
+
+});
